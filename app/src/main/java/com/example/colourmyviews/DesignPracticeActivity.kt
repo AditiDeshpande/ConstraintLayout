2afc6a33f8cf8a38d10f138d25cdb9c8946dd564
aditi <aditid11@gmail.com>
2020-05-11 17:22:02 +0530

    Used Material design... For material design check Kotlin file DesignPracticeActivity.

---
+++ app/src/main/java/com/example/colourmyviews/DesignPracticeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.colourmyviews
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DesignPracticeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practice_designing_app)
+    }
+}
